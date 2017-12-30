9c8c843bf9d625c3fb4ef090690bc38047cadaaa
Andy Tang <kiwikefei@gmail.com>
2017-12-30 21:51:19 +1300

    update lesson3

--- lesson3.php
+++ lesson3.php
@@ -25,26 +25,8 @@ function drawLine($length)
     }
     echo "\n";
 }
-function drawChristmasTree($height, $trunkHeight)
-{
-    for ($i = 1; $i <= $height; $i++) {
-        for ($j = 1; $j <= ($height - $i); $j++) {
-            echo ' ';
-        }
-        for ($k = 1; $k <= $i * 2 - 1 ; $k++) {
-            echo '*';
-        }
-        echo "\n";
-    }
-    for ($i = 1; $i <= $trunkHeight; $i++) {
-        for ($j = 2; $j <= ($height - 1); $j++) {
-            echo ' ';
-        }
-        echo "***\n";
-    }
-}
 
-// drawSquare(10);
-// drawLine(10);
+// drawSquare(6);
+// drawLine(20);
 // drawRectangle(10, 20);
-drawChristmasTree(20, 4);
+drawChristmasTree(10, 6);
--- test.php
+++ test.php
@@ -6,18 +6,22 @@ $user4 = ['name' => 'thomas', 'age' => 16];
 $users = [
     $user1, $user2, $user3, $user4
 ];
-foreach ($users as $user) {
-    if ($user['name'] == 'thomas') {
-        // $a = $real;
-        echo $user['name'] . ' age is ' . $user['age'] . "\n" ;
-    } else {
-        // $a = $notreal;
-        echo $user['name'] . "NO\n";
-    }
-    // $a = ($user['name'] == 'thomas') ? $real : $notreal;
-    if ($user['age'] < 30) {
-        echo $user['name'] . ' age is below 30 which is ' . $user['age'];
-    }
+for ($i = 1; $i <= 10; $i++) {
+    echo $i;
+    echo "\n";
 }
+// foreach ($users as $user) {
+//     if ($user['name'] == 'thomas') {
+//         // $a = $real;
+//         echo $user['name'] . ' age is ' . $user['age'] . "\n" ;
+//     } else {
+//         // $a = $notreal;
+//         echo $user['name'] . "NO\n";
+//     }
+//     // $a = ($user['name'] == 'thomas') ? $real : $notreal;
+//     if ($user['age'] < 30) {
+//         echo $user['name'] . ' age is below 30 which is ' . $user['age'];
+//     }
+// }
 
 // var_dump($d);
